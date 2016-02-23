9a06da9983731aaf200ae2c810ffbd38ace2d8d1
williammacdowell <williammacdowell@gmail.com>
2016-02-22 21:57:15 -0500

    Added thread-safe logger class

---
+++ GEDCOM/Logger.cpp
@@ -0,0 +1,47 @@
+//
+//  Logger.cpp
+//  GEDCOM
+//
+//  Created by William MacDowell on 2/22/16.
+//  Copyright © 2016 William MacDowell. All rights reserved.
+//
+
+#include "Logger.h"
+
+using namespace std;
+
+Logger::Logger(string filename)
+{
+	m_logFile.open(filename, fstream::out | fstream::app | fstream::ate);
+}
+
+Logger::~Logger()
+{
+	m_logFile.flush();
+	m_logFile.close();
+}
+
+void Logger::log(LogLevel level, string message)
+{
+	string levelStr = "";
+
+	switch (level)
+	{
+	case INFO:
+		levelStr = "INFO";
+		break;
+	case WARNING:
+		levelStr = "WARNING";
+		break;
+	case ERROR:
+		levelStr = "ERROR";
+		break;
+	default:
+		levelStr = "UNKNOWN";
+		break;
+	}
+
+	m_mutex.lock();
+	m_logFile << levelStr << "\t" << message << std::endl;
+	m_mutex.unlock();
+}
---
+++ GEDCOM/Logger.h
@@ -0,0 +1,37 @@
+//
+//  Logger.h
+//  GEDCOM
+//
+//  Created by William MacDowell on 2/22/16.
+//  Copyright © 2016 William MacDowell. All rights reserved.
+//
+#pragma once
+#include <string>
+#include <sstream>
+#include <mutex>
+#include <memory>
+#include <fstream>
+
+using namespace std;
+
+enum LogLevel
+{
+	INFO = 0,
+	WARNING,
+	ERROR
+};
+
+class Logger;
+
+class Logger
+{
+public:
+	Logger(string filename);
+	virtual ~Logger();
+
+	void log(LogLevel level, string message);
+
+private:
+	mutex		m_mutex;
+	ofstream	m_logFile;
+};
